2eda831d425783f7fb57f264895b1f3827440d45
JWriter20 <46006813+JWriter20@users.noreply.github.com>
2021-06-25 21:00:38 -0600

    Add files via upload

--- src/model/LayeredImageModel.java
+++ src/model/LayeredImageModel.java
@@ -1,14 +1,12 @@
 package model;
 
-import model.imageclasses.MultiImage;
-import model.imageclasses.MultiLayerImage;
-import model.imageclasses.Layer;
-import model.imageclasses.Image;
-import model.imageclasses.ImageTypes;
+import model.imageclasses.*;
 import model.modifications.Blurry;
 import model.modifications.Greyscale;
 import model.modifications.Sepia;
 import model.modifications.Sharpen;
+
+import java.awt.image.BufferedImage;
 
 /**
  * Class used to represent a layered image. Provides functionality to modify the image
@@ -16,70 +14,74 @@ import model.modifications.Sharpen;
  * of the image, and set the image in the first layer.
  */
 public class LayeredImageModel extends SimpleImageModel implements LayeredModel {
-  private MultiImage img = new MultiLayerImage();
+    private MultiImage img = new MultiLayerImage();
 
-  @Override
-  public void blurImage() {
-    this.mod = new Blurry();
-    this.img.applyToFirst(this.mod);
-  }
+    @Override
+    public void blurImage() {
+        this.mod = new Blurry();
+        this.img.applyToFirst(this.mod);
+    }
 
-  @Override
-  public void sepiaImage() {
-    this.mod = new Sepia();
-    this.img.applyToFirst(this.mod);
-  }
+    @Override
+    public void sepiaImage() {
+        this.mod = new Sepia();
+        this.img.applyToFirst(this.mod);
+    }
 
-  @Override
-  public void grayImage() {
-    this.mod = new Greyscale();
-    this.img.applyToFirst(this.mod);
-  }
+    @Override
+    public void grayImage() {
+        this.mod = new Greyscale();
+        this.img.applyToFirst(this.mod);
+    }
 
-  @Override
-  public void sharpImage() {
-    this.mod = new Sharpen();
-    this.img.applyToFirst(this.mod);
-  }
+    @Override
+    public void sharpImage() {
+        this.mod = new Sharpen();
+        this.img.applyToFirst(this.mod);
+    }
 
-  @Override
-  public void addLayer(String name) {
-    this.img.addLayer(new Layer(name));
-  }
+    @Override
+    public BufferedImage getViewImage() {
+        return img.getFirstVisImg();
+    }
 
-  @Override
-  public void bringToFront(String name) {
-    this.img.bringToFront(name);
-  }
+    @Override
+    public void addLayer(String name) {
+        this.img.addLayer(new Layer(name));
+    }
 
-  @Override
-  public void setFirstLayerImage(Image image) {
-    this.img.setFirstLayerImage(image);
-  }
+    @Override
+    public void bringToFront(String name) {
+        this.img.bringToFront(name);
+    }
 
-  @Override
-  public void exportFirst(String pathToFolder, ImageTypes exportType) {
-    img.exportFist(pathToFolder, exportType);
-  }
+    @Override
+    public void setFirstLayerImage(Image image) {
+        this.img.setFirstLayerImage(image);
+    }
 
-  @Override
-  public void setLayerToInvisible(String layerName) {
-    this.img.setGivenToInvisible(layerName);
-  }
+    @Override
+    public void exportFirst(String pathToFolder, ImageTypes exportType) {
+        img.exportFist(pathToFolder, exportType);
+    }
 
-  @Override
-  public void setLayerToVisible(String layerName) {
-    this.img.setGivenToVisible(layerName);
-  }
+    @Override
+    public void setLayerToInvisible(String layerName) {
+        this.img.setGivenToInvisible(layerName);
+    }
 
-  @Override
-  public void exportMultiImage(String pathToFolder, ImageTypes type) {
-    img.exportMultiLayer(pathToFolder, type);
-  }
+    @Override
+    public void setLayerToVisible(String layerName) {
+        this.img.setGivenToVisible(layerName);
+    }
+    @Override
+    public void exportMultiImage(String pathToFolder, ImageTypes type) {
+        img.exportMultiLayer(pathToFolder, type);
+    }
 
-  @Override
-  public String toString() {
-    return this.img.toString();
-  }
+    @Override
+    public String toString() {
+        return this.img.toString();
+    }
 
 }
